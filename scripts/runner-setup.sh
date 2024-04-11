a8e2a858d97eea293dbec2eb0cc56eac0b146266
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-04-11 10:50:44 +0530

    Setup Runner: Make script work again

--- scripts/runner-setup.sh
+++ scripts/runner-setup.sh
@@ -13,7 +13,7 @@ rm -rf actions-runner || true
     exit 1
   fi # Fetch version
   
-  url=https://github.com/actions/runner/releases/download/v${version}/actions-runner-linux-x64-${version}.tar.gz # Fetch URL
+  url=https://github.com/actions/runner/releases/download/v${version}/actions   -runner-linux-x64-${version}.tar.gz # Fetch URL
   url=$(echo "$url" | xargs) # Remove leading/trailing whitespace
   if ! wget -O actions-runner-linux-x64.tar.gz "${url}"; then
     echo "Failed to download the runner package"
@@ -21,6 +21,7 @@ rm -rf actions-runner || true
   fi
   
   echo "Extracting Zip"
-  tar -xvf actions-runner-linux-x64.tar.gz
+    mkdir -p actions-runner
+    tar -xvf actions-runner-linux-x64.tar.gz -C actions-runner
   echo "Removing Leftovers"
-  rm -rf actions-runner-linux-x64.tar.gz # Extract package and remove leftovers
+    rm -rf actions-runner-linux-x64.tar.gz # Extract package and remove leftovers
