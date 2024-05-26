b711c86603977a0b8eb235f3ec2a90b8c7a98134
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-05-26 13:35:46 +0530

    vscode-tunnel.sh: VSCode-tunnels script
    Simple script to set up vscode tunnels inside tmux
    
    Useful for people who only have a browser/are unable to use the crave client but still want vscode support.

---
+++ scripts/vscode-tunnel.sh
@@ -0,0 +1,22 @@
+# Copyright (C) 2024 Souhrud Reddy
+# SPDX-License-Identifier: Apache-2.0
+
+#!/bin/bash
+
+if [ -d "code-tunnel" ]; then
+  echo "skipping download!"
+else
+  echo "Installing Runner!" #Create folder and download zip
+  echo "Downloading Zip"  
+  curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli_alpine_x64_cli.tar.gz
+  echo "Extracting Zip"
+    mkdir -p code-tunnel
+    tar -xvf vscode_cli_alpine_x64_cli.tar.gz -C code-tunnel
+  echo "Removing Leftovers"
+    rm -rf vscode_cli_alpine_x64_cli.tar.gz # Extract package and remove leftovers
+fi
+
+tmux kill-session -t codetunnel || true
+tmux new-session -d -s codetunnel
+tmux send-keys -t codetunnel './code-tunnel/code tunnel --accept-server-license-terms' Enter 
+echo "Runner Started"
