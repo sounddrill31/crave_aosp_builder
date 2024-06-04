6460532adbd50c15e13b0cd80e4cf014889fadc9
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-06-04 15:33:23 +0530

    code-server.sh: Make script more verbose and fix tmux

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -2,13 +2,21 @@
 
 # Check if config file exists, download it if not
 if [ ! -f ~/.config/code-server/config.yaml ]; then
+  echo "Config not found! Downloading..."
   curl -o ~/.config/code-server/config.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/code-server/config.yaml
+else
+  echo "Config found! Skipping..."
 fi
 
 # Check if code-server command is available, install it if not
 if ! command -v code-server &> /dev/null; then
+  echo "Code-server not found! Downloading..."
   curl -fsSL https://code-server.dev/install.sh | sh
+else
+  echo "Code-server found! Skipping..."
 fi
 
 # Create a tmux window called "code-session" and run code-server inside it
-tmux new-window -n "code-session" "code-server"
+tmux new-session -d -s "code-session" 
+tmux send-keys -t "code-session" 'code-server' Enter
+echo "Created code-session!"
