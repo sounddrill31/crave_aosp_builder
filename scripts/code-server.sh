ea943ba502c612cb73733b5379523ad490ef14f3
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-06-05 12:57:20 +0530

    code-server.sh: Fix flag for new-session

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -17,6 +17,6 @@ else
 fi
 
 # Create a tmux window called "code-session" and run code-server inside it
-tmux new-session -AD -t "code-session"
+tmux new-session -AD -s "code-session"
 tmux send-keys -t "code-session" 'killall code-server ; code-server' Enter
 echo "Created code-session!"
