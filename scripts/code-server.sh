7399412e48497bf0f0138e13acabc77aab551905
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-06-05 12:54:34 +0530

    code-server.sh: Simplify  tmux command
    credit to @uvatbc

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -17,7 +17,6 @@ else
 fi
 
 # Create a tmux window called "code-session" and run code-server inside it
-tmux kill-session -t "code-session" || true
-tmux new-session -d -s "code-session" 
-tmux send-keys -t "code-session" 'code-server' Enter
+tmux new-session -AD -t "code-session"
+tmux send-keys -t "code-session" 'killall code-server ; code-server' Enter
 echo "Created code-session!"
