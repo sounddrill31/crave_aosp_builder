11f0f15287aec570a073ab77fd8b079510a79880
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-06-05 15:20:03 +0000

    Scripts: code-server: Also check if port is good

--- scripts/code-server.sh
+++ scripts/code-server.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Check if config file exists, download it if not
-if [ ! -f ~/.config/code-server/config.yaml ]; then
-  read -p "Config not found! Download? (y/n) " -n 1 -r
+# Check if config file exists and contains the correct bind address, download it if not
+if [ ! -f ~/.config/code-server/config.yaml ] || ! grep -q "bind-addr: 0.0.0.0:5899" ~/.config/code-server/config.yaml; then
+  read -p "Config not found or incorrect! Download? (y/n) " -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]] || [[ $1 == "--quiet" ]]; then
     echo "Downloading..."
+    rm ~/.config/code-server/config.yaml 2> /dev/null
     curl -o ~/.config/code-server/config.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/code-server/config.yaml
   else
     echo "Skipping..."
     exit 1
   fi
 else
-  echo "Config found! Skipping..."
+  echo "Config found and correct! Skipping..."
 fi
 
 # Check if code-server command is available, install it if not
